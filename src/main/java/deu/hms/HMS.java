0ad13ad7dde7380b9b06f2c226d3fc12881d6f4e
jimin040531 <jiminjimin0531@naver.com>
2024-11-13 23:24:04 +0900

    Initial commit

---
+++ src/main/java/deu/hms/HMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.hms;
+
+/**
+ *
+ * @author Jimin
+ */
+public class HMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
