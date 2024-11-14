f803396893946d958cb0e0039efaf58dfb87aed0
myname-jin <adsd336@daum.net>
2024-11-14 13:51:53 +0900

    cccc

--- src/main/java/deu/hms/reservation/reservationFrame.java
+++ src/main/java/deu/hms/reservation/reservationFrame.java
@@ -33,7 +33,7 @@ public class reservationFrame extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 452, Short.MAX_VALUE)
+            .addGap(0, 316, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
